94da395157a50e84bcdc88701552f6474ac2ca30
michaeljosephrice91-ops <michaeljosephrice91@gmail.com>
2026-03-08 08:17:23 +0000

    Add experimental config for missingSuspenseWithCSRBailout

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
 };
-
 module.exports = nextConfig;
