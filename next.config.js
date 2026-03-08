543420bfe5673eb5e0bb93b88d55f9061d53c9a1
michaeljosephrice91-ops <michaeljosephrice91@gmail.com>
2026-03-08 07:57:10 +0000

    Enable TypeScript build error ignoring in config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
+
 module.exports = nextConfig;
